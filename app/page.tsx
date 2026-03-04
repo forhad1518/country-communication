613b723400105e1d489b6a94ab2f3d05eecc80e3
forhad1518 <ahforhad20@gmail.com>
2026-03-04 20:31:40 +0600

    create folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div>
           <h1 className="text-center text-2xl font-bold">Welcome To Country Communication Home</h1>
-          <p className="mt-[50px]">
+          <p className="mt-12.5">
             Country Communinaction is the best EVENT Management Company in Bangladesh
           </p>
         </div>
