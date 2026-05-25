935c949cf143871d27ba20302c0bbf3a332c409f
forhad1518 <ahforhad20@gmail.com>
2026-05-25 19:11:13 +0600

    exhition event running and next avent connect data base

--- components/sections/home/TripleSlider.tsx
+++ components/sections/home/TripleSlider.tsx
@@ -104,7 +104,7 @@ export default function TripleSlider() {
               />
 
               {/* Gradient Overlay */}
-              <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-transparent to-transparent" />
+              <div className="absolute inset-0 bg-linear-to-t from-black/80 via-transparent to-transparent" />
 
               {/* Comment/Title */}
               {slide.comment && (
